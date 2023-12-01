33124af56998b60fab3bc1b9a1c66442ba307002
Abdur Rob <136497573+Abdurrobr34l@users.noreply.github.com>
2023-12-01 17:26:09 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// javascript
+// IT IS USED FOR MENU LINK ACTIVE
+document.addEventListener('DOMContentLoaded', function() {
+  var links = document.querySelectorAll('nav a');
+
+  links.forEach(function(link) {
+    link.addEventListener('click', function() {
+      // Remove active class from all links
+      links.forEach(function(innerLink) {
+        innerLink.classList.remove('active');
+      });
+
+      // Add active class to the clicked link
+      link.classList.add('active');
+    });
+  });
+});
+
+// IT IS USED TO GIVE GO TO TOP BUTTON
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// AOS ACTIVATION CODE
+<script>
+  AOS.init();
+</script>
+
+
+// jquery
+$(document).ready(function () {
+
+});
